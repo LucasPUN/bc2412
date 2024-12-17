d95a887979e0e99d8b8253c835d9e8aa2ddddc78
lucaspun <lucas_3_2_1@hotmail.com>
2024-12-17 10:17:12 +0800

    20241217

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
-# bc2412
+# Notes
+## Week 1
+- Data Types (8 Primtives)
+  - 6 of them handles numbers
+  - char & boolean
+- Operators
+  - Division (divided by 0)
+  - Math Operator (%, ++x, x++, etc)
+  - Comparison (>, <, >=, <=, !=, ==) -> Primitives
+- String (stores zero or many characters)
+  - equals(String), length(), charAt(int)
+- If, else if, else
+- for (int i = 0; i < 3; i++) {}
+- for + if (loop all elements + filtering)
+  - Searching (for + if)
 
-# welcome to the bootcmap!
-## sub-heading 
-### heading 3
---
+++ demo-helloworld/src/DemoScanner.java
@@ -0,0 +1,37 @@
+
+
+// Java Project (load built-in tools, String, Primitives)
+// proactively import non-built-in tools (Scanner)
+
+import java.util.Scanner;
+
+public class DemoScanner {
+  public static void main(String[] args) {
+    String s = "abc";
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please input a number:");
+    int day = scanner.nextInt(); // scanner.next
+    // num = num * 2;
+    // System.out.println("num=" + num); // String + int -> String
+    
+    // scanner.nextLine(); 
+
+    // System.out.println("Please input a String:");
+    // String str = scanner.nextLine(); 
+    // System.out.println("str=" + str);
+
+    // for (int i = 0; i < num; i++) {
+    //   System.out.println("hello");
+    // }
+
+    // num -> numbers of days
+    // for loop
+    int totalMinutes = 0;
+    for (int i = 0; i < day; i++) {
+      for (int j = 0; j < ; j++) {
+        
+      }
+    }
+    System.out.println(totalMinutes);
+  }
+}
